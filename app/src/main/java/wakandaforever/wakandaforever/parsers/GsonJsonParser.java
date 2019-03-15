a5227d26dfb38be574318ad69d9be14596db0d90
Rositsa Todinova <rtodinova@uni-sofia.bg>
2019-03-15 23:07:01 +0200

    Added GsonJsonParser

---
+++ app/src/main/java/wakandaforever/wakandaforever/parsers/GsonJsonParser.java
@@ -0,0 +1,37 @@
+package wakandaforever.wakandaforever.parsers;
+
+import com.google.gson.Gson;
+import com.minkov.androidapp.parsers.base.JsonParser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GsonJsonParser<T> implements JsonParser<T> {
+
+    private final Class<T> mKlass;
+    private final Class<T[]> mArrayKlass;
+    private final Gson mGson;
+
+    public GsonJsonParser(Class<T> klass, Class<T[]> arrayKlass) {
+        mKlass = klass;
+        mArrayKlass = arrayKlass;
+        mGson = new Gson();
+    }
+
+    @Override
+    public List<T> fromJsonArray(String jsonString) {
+        T[] result = mGson.fromJson(jsonString, mArrayKlass);
+        return Arrays.asList(result);
+    }
+
+    @Override
+    public T fromJson(String jsonString) {
+        return mGson.fromJson(jsonString, mKlass);
+    }
+
+    @Override
+    public String toJson(T object) {
+        return mGson.toJson(object);
+    }
+}
+
