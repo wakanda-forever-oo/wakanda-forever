b5426f958e065a493543151d787245cc2102e717
stefanmitushev <mbsa4321@gmail.com>
2019-03-15 21:12:10 +0200

    Adding backend project

---
+++ WakandaSystemServer/src/main/java/wakanda/forever/WakandaSystemServer/WakandaSystemServerApplication.java
@@ -0,0 +1,13 @@
+package wakanda.forever.WakandaSystemServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakandaSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakandaSystemServerApplication.class, args);
+	}
+
+}
