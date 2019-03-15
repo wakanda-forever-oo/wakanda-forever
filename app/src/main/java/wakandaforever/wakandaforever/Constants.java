c722e91ad0eab39a6e34de6124c37bf257907f5c
Rositsa Todinova <rtodinova@uni-sofia.bg>
2019-03-15 23:18:39 +0200

    Add class constants

---
+++ app/src/main/java/wakandaforever/wakandaforever/Constants.java
@@ -0,0 +1,11 @@
+package wakandaforever.wakandaforever;
+
+public class Constants {
+    public static final String BASE_SERVER_URL
+            = "http://192.168.2.109:8080/api";
+    public static final int SUPERHERO_NAME_MIN_LENGHT = 3;
+    public static final int SUPERHERO_NAME_MAX_LENGHT = 50;
+    public static final int SUPERHERO_SECRET_IDENTITY_MIN_LENGHT = 3;
+    public static final int SUPERHERO_SECRET_IDENTITY_MAX_LENGHT = 50;
+
+}
