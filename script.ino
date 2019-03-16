a002434441a4ef11218eb972a4ee0ef933410850
Darko Danchev <dancevdarko@gmail.com>
2019-03-16 12:12:46 +0200

    Add sensor data to Serial script

---
+++ script.ino
@@ -0,0 +1,43 @@
+#include "dht.h"
+
+
+const int soilRead = A0;
+const int dht11Read = A1;
+const int waterRead = A2; 
+const int fireRead = A3;
+
+dht DHT;
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop()
+{
+  int value = analogRead(waterRead);
+  int sensorValue = analogRead(soilRead);
+  int fireValue = analogRead(fireValue);
+  double moisture_percentage = ( 100 - ( (sensorValue/1023.00) * 100 ) );
+  DHT.read11(dht11Read);
+
+  delay(10000);
+
+  Serial.print("waterLevel=");
+  Serial.print(value);
+  Serial.print("\n");
+  Serial.print("soilMoisture=");
+  Serial.print(moisture_percentage);
+  Serial.print("\n");
+  Serial.print("humidity=");
+  Serial.print(DHT.humidity);
+  Serial.print("\n");
+  Serial.print("temperature=");
+  Serial.print(DHT.temperature); 
+  Serial.print("\n");
+  Serial.print("fire=");
+  Serial.print(fireValue);
+  Serial.print("\n");
+  delay(1000);
+}
