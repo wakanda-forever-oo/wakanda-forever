6a8f383344f6068069b202b1850ad8b3dfe95ba3
stefanmitushev <mbsa4321@gmail.com>
2019-03-16 20:09:17 +0200

    Added default constructor for illnes

--- WakandaSystemServer/src/main/java/wakanda/forever/WakandaSystemServer/model/illness/Illness.java
+++ WakandaSystemServer/src/main/java/wakanda/forever/WakandaSystemServer/model/illness/Illness.java
@@ -9,11 +9,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import wakanda.forever.WakandaSystemServer.dto.illness.IllnessDto;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Illness {
 
 	@Id	
